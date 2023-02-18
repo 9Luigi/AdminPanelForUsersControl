2bd34ac8f29aeba6c3aba9a050bc5e0864be2a08
Kudrik Roman <fastromankudrik@gmail.com>
2023-02-18 23:28:58 +0600

    Started script for print users list on admin demand

--- CarsRentApplication/wwwroot/js/admin.js
+++ CarsRentApplication/wwwroot/js/admin.js
@@ -1,18 +1,15 @@
-const userTable = document.getElementById("usersTable");
+const mainGrid = document.getElementById("main");
 
 document.getElementById("DownloadUsers").addEventListener("click", async () => {
     const response = await fetch("/admin/getAllUsers");
     if (response.ok === true) {
         const responseText = await response.json();
-        userTable.innerHTML += "<tr>";
-        responseText.forEach(element => {
-            userTable.innerHTML += "<tr>";
-            userTable.innerHTML += "<td>" + element.name + "</td>";
-            userTable.innerHTML += "<td>" + element.name + "</td>";
-            userTable.innerHTML += "<td>" + element.name + "</td>";
-            userTable.innerHTML += "<td>" + element.name + "</td>";
-            userTable.innerHTML += "</tr>";
-            console.log(element.name);
+        var table_str = '<table><thead><tr><th>Name</th><th>Email</th></tr></thead><tbody>';
+        responseText.forEach(user => {
+            table_str += '<tr><td>' + user.name + '</td><td>' + user.email + '</td></tr>';
         });
+        table_str += '</tbody></table>'
+        mainGrid.innerHTML = table_str;
     }
 })
+
